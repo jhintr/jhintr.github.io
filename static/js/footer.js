331e039cd9a33d418686f5f4659dbb6c9ef17f8e
Zhi Shao <jhintr@gmail.com>
2024-01-06 13:29:52 +0800

    update footer.js & style.css

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,17 +1,14 @@
-/* last update: 2024-01-01 */
+/* last update: 2024-01-06 */
 
 const year = new Date().getFullYear();
 
 const copy = document.createElement("span");
-copy.innerHTML = `&copy;${year} <a class="text-secondary" href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh-hans" target="_blank">Cc-By-Sa-4.0↗</a>`;
+copy.innerHTML = `&copy; ${year} <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh-hans" target="_blank">Cc-By-Sa-4.0↗</a>`;
 
 const mail = document.createElement("span");
-mail.innerHTML = `<a class="ms-2 text-secondary" href="mailto:sadhu@ehipassa.org">Contact me↗</a>`;
+mail.innerHTML = `<a class="ms-2" href="mailto:sadhu@ehipassa.org">Contact me↗</a>`;
 
 const p = document.getElementById("footer");
-p.classList.add("text-secondary");
-p.style.fontSize = "0.75rem";
-p.style.fontVariant = "small-caps";
 p.appendChild(copy);
 p.appendChild(mail);
 
