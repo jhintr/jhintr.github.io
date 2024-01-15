719d5ffbb13f191e218f423c4e862ac4c8199a9f
Zhi Shao <jhintr@gmail.com>
2024-01-15 17:32:08 +0800

    update 2 js

--- static/js/pj2snp.js
+++ static/js/pj2snp.js
@@ -1,4 +1,4 @@
-/* last update: 2023-04-06 */
+/* last update: 2024-01-15 */
 /* 说明：此文件加载在「义释」及「第一义光」 */
 /* 在其标题5 上附加超链接，以转向到经集的相关偈颂 */
 
@@ -15,6 +15,6 @@ if (url.includes('atthakatha/paramatthajotika')) {
 }
 h5El.forEach(h => {
     const id = h.id.split('-')[0];
-    const icon = `<span title="查看经集">&RightArrowLeftArrow;</span>`;
-    h.innerHTML = `<a href="${url}#${id}">${h.innerHTML} ${icon}</a>`;
+    const icon = `<span title="查看经集">↗</span>`;
+    h.innerHTML = `${h.innerHTML} <a href="${url}#${id}">${icon}</a>`;
 });
--- static/js/snp2pj2.js
+++ static/js/snp2pj2.js
@@ -1,4 +1,4 @@
-/* last update: 2023-10-15 */
+/* last update: 2024-01-15 */
 /* 说明：此文件加载在「经集」  */
 /* 在其标题5 上附加超链接，以转向到「第一义光」的相关偈颂 */
 
@@ -8,6 +8,6 @@ url = url.replace('khuddaka/suttanipata', 'atthakatha/paramatthajotika');
 
 h5El_pj.forEach(h => {
     const id = h.id;
-    const icon = `<span title="查看义注">&RightArrowLeftArrow;</span>`;
+    const icon = `<span title="查看义注">↘</span>`;
     h.innerHTML = `${h.innerHTML} <a href="${url}#${id}">${icon}</a>`;
 });
