bd6275acc1c20fa3fddc40152b9f6dee22a0498a
Zhi Shao <jhintr@gmail.com>
2024-07-10 08:31:25 +0800

    update vm 1.76

--- static/js/vm2c.js
+++ static/js/vm2c.js
@@ -1,11 +1,15 @@
-/* last update: 2024-06-29 */
-/* 说明：此文件加载在「义注·清净道论」 */
-/* 在其标题5 上附加超链接，转向到叶均的译本 */
+/* last update: 2024-07-10 */
+/* 说明：此文件加载在「清净道论」巴利及汉译 */
+/* 在其标题5 上附加超链接，转向到汉译或巴利 */
 
 const h5El = document.querySelectorAll('h5');
 let url = window.location.pathname;
-url = url.replace('atthakatha/', '');
+if (url.includes('atthakatha/')) {
+    url = url.replace('atthakatha/', '');
+} else {
+    url = url.replace('visuddhimagga/', 'atthakatha/visuddhimagga/')
+}
 h5El.forEach(h => {
-    const icon = `<small title="查看汉译">↗</small>`;
+    const icon = `<small>↗</small>`;
     h.innerHTML = `${h.innerHTML} <a href="${url}#${h.id}">${icon}</a>`;
 });
