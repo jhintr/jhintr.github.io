0a9c0f56f4888e7eefafe3d0526e60bab931d508
Zhi Shao <jhintr@gmail.com>
2024-07-15 09:28:25 +0800

    prepare Niddesa

--- static/js/pj2snp.js
+++ static/js/pj2snp.js
@@ -1,18 +1,10 @@
-/* last update: 2024-01-18 */
-/* 说明：此文件加载在「义释」及「第一义光」 */
+/* last update: 2024-07-14 */
+/* 说明：此文件加载在「第一义光」 */
 /* 在其标题5 上附加超链接，以转向到经集的相关偈颂 */
 
 const h5El = document.querySelectorAll('h5');
 let url = window.location.pathname;
-if (url.includes('atthakatha/paramatthajotika')) {
-    url = url.replace('atthakatha/paramatthajotika', 'khuddaka/suttanipata');
-} else if (url.includes('/1')) { // 返回经集·八颂品
-    url = url.replace('niddesa/1', 'suttanipata/4');
-} else if (url.includes('/223')) { // 返回经集·犀牛角经
-    url = url.replace('niddesa/223', 'suttanipata/103');
-} else { // 返回经集·彼岸道品
-    // to-do
-}
+url = url.replace('atthakatha/paramatthajotika', 'khuddaka/suttanipata');
 h5El.forEach(h => {
     const id = h.id.split('-')[0];
     const icon = `<small title="查看经集">↗</small>`;
