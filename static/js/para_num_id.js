36aabd9a2024134624dc3390a2b870fb070dbb17
Zhi Shao <jhintr@proton.me>
2026-05-18 19:59:47 +0800

    update netti 4

--- static/js/para_num_id.js
+++ static/js/para_num_id.js
@@ -1,4 +1,4 @@
-/* last update: 2026-04-16 */
+/* last update: 2026-05-18 */
 /* 说明：若 p 段落以 <加粗数字.> 开始，就设置此段落 id 为数字 */
 
 const articleParagraphs = document.querySelectorAll('article p');
@@ -7,7 +7,7 @@ articleParagraphs.forEach(p => {
   const firstChild = p.firstElementChild;
 
   if (firstChild && (firstChild.tagName === 'B' || firstChild.tagName === 'STRONG')) {
-    const match = firstChild.textContent.trim().match(/^(\d+)\./);
+    const match = firstChild.textContent.trim().match(/^(\d+[a-zA-Z]?)\./);
     if (match) {
       const idValue = match[1];
       p.id = idValue;
